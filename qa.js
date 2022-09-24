2fe032797fb858adfeac222d5599a251b5e53f63
modudibai <113973285+modudibai@users.noreply.github.com>
2022-09-24 18:17:34 +0800

    Create qa.js

---
+++ qa.js
@@ -0,0 +1,13 @@
+{
+  rollAnswer: {
+    {qid: 1, answer: 1},
+    {qid: 2, answer: 2},
+    {qid: 3, answer: 1}
+  },
+  GuildAnswer: {
+    {qid: 1, answer: 1}
+  },
+  NomalAnswer: {
+    {qid: 1, answer: 1}
+  }
+}
